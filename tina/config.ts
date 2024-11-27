6c13296a064be0ccf7a03450d5f1f56707c313fa
nomoi-gr <info.nomoi.gr@gmail.com>
2024-11-27 13:50:10 +0200

    with tina

---
+++ tina/config.ts
@@ -0,0 +1,131 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "static",
+      static: false,
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "docs",
+        label: "Docs",
+        path: "content/docs",
+        fields: [
+          {
+            name: 'slug',
+            label: 'Slug',
+            type: 'string',
+            required: true,
+            description: 'slug field should be art\<num\>',
+          },
+          {
+            name: 'prev',
+            label: 'Prev',
+            type: 'string',
+            required: true,
+            description: 'prev field should be art\<num\>',
+          },
+          {
+            name: 'next',
+            label: 'Next',
+            type: 'string',
+            required: true,
+            description: 'next field should be art\<num\>',
+          },         
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+            templates: [
+              {
+                name: 'callout',
+                label: 'Callout',
+                match: {
+                  start: '{{<',
+                  end: '>}}',
+                },
+                fields: [
+                  {
+                    name: "children",
+                    type: 'rich-text',
+                    parser: {
+                        type: 'markdown',
+                        skipEscaping: 'all', // this allows {{< to go unescaped
+                        },
+                  }
+                ],
+              },
+              {
+                name: 'details',
+                label: 'Details',
+                match: {
+                  start: '{{<',
+                  end: '>}}',
+                },
+                fields: [
+                  {
+                    name: '_value',
+                    label: 'value',
+                    type: 'string',
+                    required: false,
+                  },
+                  {
+                    name: 'title',
+                    label: 'Title',
+                    type: 'string',
+                    required: true,
+                  },
+                  {
+                    name: 'closed',
+                    label: 'Close',
+                    type: 'boolean',
+                    required: false,
+                  },
+                  {
+                    name: "children",
+                    type: "rich-text",
+                    parser: {
+                      type: 'markdown',
+                      skipEscaping: 'all', // this allows {{< to go unescaped
+                      },                  
+                  }
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
